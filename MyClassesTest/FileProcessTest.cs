9d00f0d4366bd4a06792b85b11962e5b81b56f72
gadasilvas <gabriel.seles@engdb.com.br>
2021-03-10 23:22:53 -0300

    Adicionar arquivos de projeto.

---
+++ MyClassesTest/FileProcessTest.cs
@@ -0,0 +1,40 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using MyClasses;
+using System;
+
+namespace MyClassesTest
+{
+    [TestClass]
+    public class FileProcessTest
+    {
+        [TestMethod]
+        public void FileNameDoesExists()
+        {
+            FileProcess fileProcess = new FileProcess();
+            bool fromCall;
+
+            fromCall = fileProcess.FileExists(@"C:\checkOS.txt");
+
+
+            Assert.IsTrue(fromCall);
+        }
+
+        [TestMethod]
+        public void FileNameDoesNotExists()
+        {
+            FileProcess fileProcess = new FileProcess();
+            bool fromCall;
+
+            fromCall = fileProcess.FileExists(@"C:\checkOS2.txt");
+
+
+            Assert.IsFalse(fromCall);
+        }
+
+        [TestMethod]
+        public void FileNameNullOrEmpty()
+        {
+            Assert.Inconclusive();
+        }
+    }
+}
