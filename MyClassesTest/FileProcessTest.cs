f7b14e5e158bf010d0c3314b1cc663aa80461e68
gadasilvas <gabriel.seles@engdb.com.br>
2021-03-11 22:43:26 -0300

    Testes unitários

--- MyClassesTest/FileProcessTest.cs
+++ MyClassesTest/FileProcessTest.cs
@@ -1,20 +1,44 @@
 ﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
 using MyClasses;
 using System;
+using System.Configuration;
+using System.IO;
 
 namespace MyClassesTest
 {
     [TestClass]
     public class FileProcessTest
     {
+
+        private const string BAD_FILE_NAME = "BadFile";
+        private string _GoodFileName;
+
+        public TestContext TestContext { get; set; }
+
+        public void SetGoodFileName()
+        {
+            _GoodFileName = ConfigurationManager.AppSettings["GoodFileName"];
+            if (_GoodFileName.Contains("[AppPath]"))
+            {
+                _GoodFileName = _GoodFileName.Replace("[AppPath]", 
+                    Environment.GetFolderPath(Environment.SpecialFolder.ApplicationData));
+            }
+        }
+
         [TestMethod]
         public void FileNameDoesExists()
         {
             FileProcess fileProcess = new FileProcess();
             bool fromCall;
 
-            fromCall = fileProcess.FileExists(@"C:\checkOS.txt");
+            SetGoodFileName();
+            TestContext.WriteLine("Creating File");
+            File.AppendAllText(_GoodFileName, "Some Text");
+            TestContext.WriteLine("Testing File");
 
+            fromCall = fileProcess.FileExists(_GoodFileName);
+            TestContext.WriteLine("Deleting File");
+            File.Delete(_GoodFileName);
 
             Assert.IsTrue(fromCall);
         }
@@ -25,16 +49,35 @@ namespace MyClassesTest
             FileProcess fileProcess = new FileProcess();
             bool fromCall;
 
-            fromCall = fileProcess.FileExists(@"C:\checkOS2.txt");
+            fromCall = fileProcess.FileExists(BAD_FILE_NAME);
 
 
             Assert.IsFalse(fromCall);
         }
 
         [TestMethod]
+        [ExpectedException(typeof(ArgumentNullException))]
         public void FileNameNullOrEmpty()
         {
-            Assert.Inconclusive();
+            FileProcess fp = new FileProcess();
+            fp.FileExists("");
+        }
+
+        [TestMethod]
+        public void FileNameNullOrEmptyTryCatch()
+        {
+            FileProcess fp = new FileProcess();
+            try
+            {
+                fp.FileExists("");
+            }
+            catch (ArgumentException)
+            {
+
+                return;
+            }
+
+            Assert.Fail("Fail");
         }
     }
 }
